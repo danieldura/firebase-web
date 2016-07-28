025ce456384612c915a6151cf30fe590c88a34f7
ddura <ddura@mobimento.com>
2016-07-28 09:39:14 +0200

    Create new records on firebase database

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-var rootRef = new Firebase('https://ddura-jugadores.firebaseio.com/');
-rootRef.on("value", function(snapshot){
-	console.log(snapshot.val())
-	var data = snapshot.val();
+$(document).ready(function(){
+	var rootRef = new Firebase('https://ddura-jugadores.firebaseio.com/');
+	rootRef.on("value", function(snapshot){
+		console.log(snapshot.val())
+		var data = snapshot.val();
 
-	$("#playersTable tbody").empty();
+		$("#playersTable tbody").empty();
 
-	var row = "";
+		var row = "";
 
-	for (player in snapshot.val()){
-		console.log(player, ',',data[player]);
+		for (player in snapshot.val()){
+			console.log(player, ',',data[player]);
 
-		row += "<tr>"+
-				"<td>" + player + "</td>" +
-				"<td>" + data[player].mail + "</td>" +
-				"<td>" + data[player].number + "</td>" +
-				"<td>" + data[player].position + "</td>" +
-			"<tr>"
-	}
-	$("#playersTable tbody").append(row);
-}, function(errorObject){
-	console.log("The read failed: " + errorObject.code);
+			row += "<tr>"+
+					"<td>" + player + "</td>" +
+					"<td>" + data[player].mail + "</td>" +
+					"<td>" + data[player].number + "</td>" +
+					"<td>" + data[player].position + "</td>" +
+				"<tr>"
+		}
+		$("#playersTable tbody").append(row);
+	}, function(errorObject){
+		console.log("The read failed: " + errorObject.code);
+	});
+
+	$("#btnSend").click(function(){
+		var fullname = $("#fullName").val();
+
+		var dataPlayer = {
+			name: fullName,
+			mail: $("#mail").val(),
+			number: $("#number").val(),
+			position: $("#position option:selected").text()
+		}
+
+		rootRef.child(fullname).set(dataPlayer);
+	})
 });
-
-$("#btnSend").click(function(){
-	var fullname = $("#fullName").val();
-
-	var dataPlayer = {
-		name: fullName,
-		mail: $("#mail").val(),
-		number: $("#number")val(),
-		position: $("position option:selected").text()
-	}
-})
