856333246f3a2ee437c61adddfda2f6fb63b0a97
ddura <danieldura7@gmail.com>
2016-07-29 11:39:20 +0200

    Update records from Firebase

---
+++ ReadMe.md
@@ -0,0 +1,25 @@
+#FireBase CRUD web
+![image](http://www.programwitherik.com/content/images/2015/06/Firebase.png)
+
+##Table of contents
+
+##Abstract
+CRUD example for firebase
+create, read, update and delete are the four basic functions of persistent storage.
+
+##Tools
+* Jquery
+* BootStrap
+* HTML5
+
+
+
+
+##Description
+What is Firebase?
+
+Firebase is a real time application platform. It allows developers to build rich, collaborative applications quickly using just client-side code. Applications built with Firebase:
+
+Can be built rapidly
+Update in realtime out-of-the-box
+Remain responsive even when a network connection is unavailable
--- app.js
+++ app.js
@@ -1,78 +1,109 @@
 $(document).ready(function(){
-	var rootRef = new Firebase('https://ddura-jugadores.firebaseio.com/');
-	rootRef.on("value", function(snapshot){
-		// console.log(snapshot.val())
-		var data = snapshot.val();
+		var rootRef = new Firebase('https://ddura-jugadores.firebaseio.com/');
+		rootRef.on("value", function(snapshot){
+			// console.log(snapshot.val())
+			var data = snapshot.val();
 
-		$("#playersTable tbody").empty();
+			$("#playersTable tbody").empty();
 
-		var row = "";
+			var row = "";
 
-		for (player in snapshot.val()){
-			//console.log(player, ',',data[player]);
+			for (player in snapshot.val()){
+				//console.log(player, ',',data[player]);
 
-			row += "<tr>"+
-					"<td class=\"playerName\">" + player + "</td>" +
-					"<td>" + data[player].mail + "</td>" +
-					"<td>" + data[player].number + "</td>" +
-					"<td>" + data[player].position + "</td>" +
-					"<td> <div class=\"btnDelete btn btn-danger glyphicon glyphicon-remove\"></div></td>" +
-				"</tr>"
-		}
-		$("#playersTable tbody").append(row);
+				row += "<tr>"+
+						"<td class=\"playerName\">" + player + "</td>" +
+						"<td class=\"mail\">" + data[player].mail + "</td>" +
+						"<td class=\"number\">" + data[player].number + "</td>" +
+						"<td class=\"position\">" + data[player].position + "</td>" +
+						"<td> <div class=\"btnEdit btn btn-warning glyphicon glyphicon-edit\"></div> </td>" +
+						"<td> <div class=\"btnDelete btn btn-danger glyphicon glyphicon-remove\"></div></td>" +
+					"</tr>"
+			}
+			$("#playersTable tbody").append(row);
 
-		row="";
-// ##################################################	
-// ###########  Delete record from firebase #########
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+			row="";
+	// ##################################################	
+	// ###########  Delete record from firebase #########
+	// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-		$(".btnDelete").click(function(){
-			console.log('clicked')
-			var selectedPlayer = $(this).closest("tr")
+			$(".btnDelete").click(function(){
+				console.log('clicked')
+				var selectedPlayer = $(this).closest("tr")
+					.find(".playerName")
+					.text();
+
+				console.log(selectedPlayer);
+				rootRef.child(selectedPlayer).remove();
+			})
+
+
+	// ##################################################	
+	// ###########  Edit record from firebase ###########
+	// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+			$(".btnEdit").click(function(){
+				console.log('Button edit clicked');
+				var selectedPlayer = $(this).closest("tr")
 				.find(".playerName")
 				.text();
 
-			console.log(selectedPlayer);
-			rootRef.child(selectedPlayer).remove();
-		})
+				$("#fullName").val($(this).closest("tr").find(".playerName").text());
+				$("#mail").val($(this).closest("tr").find(".mail").text());
+				$("#number").val($(this).closest("tr").find(".number").text());
+				$("#position").val($(this).closest("tr").find(".position").text());
+				$("#btnSend").text("Actualizar").removeClass("btn-primary").addClass("btn-warning").unbind("click").click(function(){
+					rootRef.child(selectedPlayer).update({
+						mail: $("#mail").val(),
+						number: $("#number").val(),
+						position: $("#position option:selected").text()				
+					},function(){
+						$("#fullName").val("");
+						$("#mail").val("");
+						$("#number").val("");
+						$("#position").val("");
+						$("#btnSend").text("Enviar").removeClass("btn-warning").addClass("btn-primary").unbind("click").click(sendData);
+					})
 
+				});
+
+			})
+
+			$("#btnSend").click(sendData);
 
 
 	}, function(errorObject){
-		console.log("The read failed: " + errorObject.code);
+			console.log("The read failed: " + errorObject.code);
 	});
-// ##################################################	
-// ###########  Sending data to firebase ############
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    $("#btnSend").click(function() {
-        var fullName = $("#fullName").val();
 
-        var dataPlayer = {
-            name: fullName,
-            mail: $("#mail").val(),
-            number: $("#number").val(),
-            position: $("#position option:selected").text()
-        }
+	// ##################################################	
+	// ###########  Sending data to firebase ############
+	// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+	function sendData() {
+	    var fullName = $("#fullName").val();
 
-        var onComplete = function(error){
-        	if (error){
-        		console.log(error,'La sincronización falló');
-        	}else{
-        		console.log(error,'La sincronización ha sido exitosa');
-        	}
-        }
+	    var dataPlayer = {
+	        name: fullName,
+	        mail: $("#mail").val(),
+	        number: $("#number").val(),
+	        position: $("#position option:selected").text()
+	    }
 
-        rootRef.once('value', function(snapshot){
-            if(snapshot.hasChild(fullName)){
-                $('#myModal').modal('show');
-            } else {
-                rootRef.child(fullName).set(dataPlayer, onComplete);
-            }
+	    var onComplete = function(error){
+	    	if (error){
+	    		console.log(error,'La sincronización falló');
+	    	}else{
+	    		console.log(error,'La sincronización ha sido exitosa');
+	    	}
+	    }
 
-        })
+	    rootRef.once('value', function(snapshot){
+	        if(snapshot.hasChild(fullName)){
+	            $('#myModal').modal('show');
+	        } else {
+	            rootRef.child(fullName).set(dataPlayer, onComplete);
+	        }
 
+	    })      
 
-       
-
-    })
+	}
 });
